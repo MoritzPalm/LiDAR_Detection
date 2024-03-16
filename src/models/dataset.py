5f252f7c3c6811385247df6b41b8717491ab62ba
Moritz Palm <moritz.palm@web.de>
2024-03-16 19:23:56 +0100

    fixed errors in data path

--- src/models/dataset.py
+++ src/models/dataset.py
@@ -4,7 +4,9 @@ from PIL import Image
 from torchvision import tv_tensors
 from torch.utils.data import Dataset
 
-from src.utils import read_labels, get_absolute_coords
+from src.utils import read_labels, get_relative_coords
+
+# TODO: include data augmentation and other transformations
 
 
 class LiDARDataset(Dataset):
@@ -19,21 +21,19 @@ class LiDARDataset(Dataset):
 
     def __getitem__(self, idx: int):
         idx = str(idx).zfill(6)  # filling with zeros to match the 6 digit file name
-        img_path = os.path.join(self.img_dir, f"frame_{idx}.npy")
-        label_path = os.path.join(self.labels_dir, f"frame_{idx}.npy")
+        img_path = os.path.join(self.img_dir, f"frame_{idx}.PNG")
+        label_path = os.path.join(self.labels_dir, f"frame_{idx}.txt")
         image = tv_tensors.Image(Image.open(img_path))
         labels = read_labels(Path(label_path))
-        width = image.shape[0]
-        height = image.shape[1]
-        abs_labels = []
+        rel_labels = []
         classes = []
         for label in labels:
-            class_, x, y, w, h = get_absolute_coords(label, width, height)
-            abs_labels.append([x, y, w, h])
+            class_, x, y, w, h = get_relative_coords(label)
+            rel_labels.append([x, y, w, h])
         bboxes = tv_tensors.BoundingBoxes(
-            abs_labels,
+            rel_labels,
             format=tv_tensors.BoundingBoxFormat.XYWH,
-            canvas_size=(width, height),
+            canvas_size=(image.shape[0], image.shape[1]),
         )
         target = {"classes": classes, "bboxes": bboxes}
         return image, target
