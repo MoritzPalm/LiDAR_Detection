e878a2f80026c984b4973e23181f4162b6e2b4cf
Moritz Palm <moritz.palm@web.de>
2024-03-19 08:59:48 +0100

    added train-val split to dataset

--- src/models/dataset.py
+++ src/models/dataset.py
@@ -1,8 +1,12 @@
 import os
+import yaml
 from pathlib import Path
 from PIL import Image
-from torchvision import tv_tensors
+import numpy as np
+import torch
+from torchvision import tv_tensors, transforms
 from torch.utils.data import Dataset
+from torch.utils.data.sampler import SubsetRandomSampler
 
 from src.utils import read_labels, get_relative_coords
 
@@ -37,4 +41,53 @@ class LiDARDataset(Dataset):
             canvas_size=(image.shape[0], image.shape[1]),
         )
         target = {"classes": classes, "bboxes": bboxes}
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            target = self.target_transform(target)
         return image, target
+
+
+transform = transforms.Compose(
+    [
+        #transforms.Resize((256, 256)),
+        #transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+)
+
+target_transform = transforms.Compose(
+    [
+        #transforms.ToTensor(),
+    ]
+)
+
+
+def make_loaders(dataset, batch_size=64, validation_split=.2) \
+        -> tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader]:
+    """
+    Returns a DataLoader for the given dataset
+    :param validation_split:
+    :param dataset:
+    :param batch_size:
+    :return:
+    """
+    random_seed = 42
+    np.random.seed(random_seed)
+    validation_split = .2
+
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+
+    split = int(np.floor(validation_split * dataset_size))
+    np.random.shuffle(indices)
+    train_indices, val_indices = indices[split:], indices[:split]
+
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
+
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                               sampler=train_sampler, shuffle=True)
+    validation_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                                    sampler=valid_sampler, shuffle=False)
+    return train_loader, validation_loader
