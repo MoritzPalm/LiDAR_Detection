3e71ce392bf03bb0c9e62c3a63a2b55cca6cb2de
Moritz Palm <moritz.palm@web.de>
2024-04-02 18:10:24 +0200

    added wandb integration and passing of all relevant paramenters by config

--- src/models/cnn_v1_lightning.py
+++ src/models/cnn_v1_lightning.py
@@ -1,21 +1,32 @@
 import lightning as L
+from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint, LearningRateMonitor
 import torch.nn.functional as F
 from torch import optim
-from torchvision.models import resnet50
+from torchvision.models import resnet50, ResNet50_Weights
+from torchmetrics import Accuracy
+import munch
+import yaml
+from pathlib import Path
 
 from src.models.cnn_v1 import Cnn
 from src.models.dataset import LiDARDataset, make_loaders, transforms
 
+config = munch.munchify(yaml.load(open("../../config.yaml"), Loader=yaml.FullLoader))
+
 
 class CnnLight(L.LightningModule):
-    def __init__(self, baseModel, lr):
+    def __init__(self, config):
         super().__init__()
-        self.model = Cnn(baseModel, 8)
-        self.lr = lr
+        self.config = config
+
+        weights = ResNet50_Weights.DEFAULT if self.config.use_pretrained_weights else None
+        self.baseModel = resnet50(weights=weights)
+        self.model = Cnn(self.baseModel, self.config.num_classes)
+        self.acc_fn = Accuracy(task="multiclass", num_classes=self.config.num_classes)
 
     def training_step(self, batch, batch_idx):
         img, classes, bboxes = batch
-        # TODO: reshape x
         classes_pred, bboxes_pred = self.model(img)
         loss = self.compute_loss(classes_pred, bboxes_pred, classes, bboxes)
         return loss
@@ -29,30 +40,42 @@ class CnnLight(L.LightningModule):
         return classLoss + regLoss
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=self.lr)
+        optimizer = optim.Adam(self.parameters(), lr=self.config.max_lr)
         return optimizer
 
 
 if __name__ == "__main__":
-    DATA_PATH = "../../data/NAPLab-LiDAR"
-    IMAGE_PATH = "../../data/NAPLab-LiDAR/images"
-    LABEL_PATH = "../../data/NAPLab-LiDAR/labels_yolo_v1.1"
-    with open(f'{DATA_PATH}/train.txt', 'r') as f:
-        train_files = f.read().split('\n')
-        train_files.remove('')
-
     dataset = LiDARDataset(
         "../../data/NAPLab-LiDAR/images",
         "../../data/NAPLab-LiDAR/labels_yolo_v1.1",
         transform=transforms,
     )
-    train_loader, validation_loader = make_loaders(dataset, batch_size=1, validation_split=.2)
+    train_loader, validation_loader = make_loaders(dataset, batch_size=config.batch_size, validation_split=.2)
 
-    resnet = resnet50(pretrained=True)
-    for param in resnet.parameters():
-        param.requires_grad = False
+    if config.checkpoint_path:
+        model = CnnLight.load_from_checkpoint(checkpoint_path=config.checkpoint_path, config=config)
+        print("Loading weights from checkpoint...")
+    else:
+        model = CnnLight(config)
 
-    model = CnnLight(resnet, 1e-3)
-    trainer = L.Trainer(max_epochs=10)
+    trainer = L.Trainer(devices=config.devices,
+                        max_epochs=config.max_epochs,
+                        check_val_every_n_epoch=config.check_val_every_n_epoch,
+                        enable_progress_bar=config.enable_progress_bar,
+                        precision="bf16-mixed",
+                        # deterministic=True,
+                        logger=WandbLogger(project=config.wandb_project, name=config.wandb_experiment_name,
+                                           config=config),
+                        callbacks=[
+                            EarlyStopping(monitor="val/acc", patience=config.early_stopping_patience, mode="max",
+                                          verbose=True),
+                            LearningRateMonitor(logging_interval="step"),
+                            ModelCheckpoint(dirpath=Path(config.checkpoint_folder, config.wandb_project,
+                                                         config.wandb_experiment_name),
+                                            filename='best_model:epoch={epoch:02d}-val_acc={val/acc:.4f}',
+                                            auto_insert_metric_name=False,
+                                            save_weights_only=True,
+                                            save_top_k=1),
+                        ])
     trainer.fit(model, train_loader)
     trainer.test(model, validation_loader)
