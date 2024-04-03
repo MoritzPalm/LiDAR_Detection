5f60fb27eb0a69705b802cb0e013ca1047e56188
Moritz Palm <moritz.palm@web.de>
2024-04-03 20:04:57 +0200

    restructured code to train from separate file

--- src/models/cnn_v1_lightning.py
+++ src/models/cnn_v1_lightning.py
@@ -1,7 +1,7 @@
-import lightning as L
+import pytorch_lightning as L
 import torch
-from lightning.pytorch.loggers import WandbLogger
-from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint, LearningRateMonitor
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, LearningRateMonitor
 import torch.nn.functional as F
 from torch import optim
 from torchvision.models import resnet50, ResNet50_Weights
@@ -12,8 +12,7 @@ from pathlib import Path
 
 from src.models.cnn_v1 import Cnn
 from src.models.dataset import LiDARDataset, make_loaders, transforms
-
-config = munch.munchify(yaml.load(open("../../config.yaml"), Loader=yaml.FullLoader))
+from src.models.multiboxloss import SSDMultiboxLoss
 
 
 class SSD(L.LightningModule):
@@ -46,38 +45,3 @@ class SSD(L.LightningModule):
         return optimizer
 
 
-if __name__ == "__main__":
-    dataset = LiDARDataset(
-        "../../data/NAPLab-LiDAR/images",
-        "../../data/NAPLab-LiDAR/labels_yolo_v1.1",
-        transform=transforms,
-    )
-    train_loader, validation_loader = make_loaders(dataset, batch_size=config.batch_size, validation_split=.2)
-
-    if config.checkpoint_path:
-        model = SSD.load_from_checkpoint(checkpoint_path=config.checkpoint_path, config=config)
-        print("Loading weights from checkpoint...")
-    else:
-        model = SSD(config)
-
-    trainer = L.Trainer(devices=config.devices,
-                        max_epochs=config.max_epochs,
-                        check_val_every_n_epoch=config.check_val_every_n_epoch,
-                        enable_progress_bar=config.enable_progress_bar,
-                        precision="bf16-mixed",
-                        # deterministic=True,
-                        logger=WandbLogger(project=config.wandb_project, name=config.wandb_experiment_name,
-                                           config=config),
-                        callbacks=[
-                            EarlyStopping(monitor="val/acc", patience=config.early_stopping_patience, mode="max",
-                                          verbose=True),
-                            LearningRateMonitor(logging_interval="step"),
-                            ModelCheckpoint(dirpath=Path(config.checkpoint_folder, config.wandb_project,
-                                                         config.wandb_experiment_name),
-                                            filename='best_model:epoch={epoch:02d}-val_acc={val/acc:.4f}',
-                                            auto_insert_metric_name=False,
-                                            save_weights_only=True,
-                                            save_top_k=1),
-                        ])
-    trainer.fit(model, train_loader)
-    trainer.test(model, validation_loader)
---
+++ src/train.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+import munch
+import yaml
+import pytorch_lightning as L
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, LearningRateMonitor
+
+from models.dataset import LiDARDataset, make_loaders, transforms
+from models.cnn_v1_lightning import SSD
+
+
+config = munch.munchify(yaml.load(open("../config.yaml"), Loader=yaml.FullLoader))
+
+
+if __name__ == "__main__":
+    dataset = LiDARDataset(
+        "../data/NAPLab-LiDAR/images",
+        "../data/NAPLab-LiDAR/labels_yolo_v1.1",
+        transform=transforms,
+    )
+    train_loader, validation_loader = make_loaders(dataset, batch_size=config.batch_size, validation_split=.2)
+
+    if config.checkpoint_path:
+        model = SSD.load_from_checkpoint(checkpoint_path=config.checkpoint_path, config=config)
+        print("Loading weights from checkpoint...")
+    else:
+        model = SSD(config)
+
+    trainer = L.Trainer(devices=config.devices,
+                        max_epochs=config.max_epochs,
+                        check_val_every_n_epoch=config.check_val_every_n_epoch,
+                        enable_progress_bar=config.enable_progress_bar,
+                        precision="bf16-mixed",
+                        # deterministic=True,
+                        logger=WandbLogger(project=config.wandb_project, name=config.wandb_experiment_name,
+                                           config=config),
+                        callbacks=[
+                            EarlyStopping(monitor="val/acc", patience=config.early_stopping_patience, mode="max",
+                                          verbose=True),
+                            LearningRateMonitor(logging_interval="step"),
+                            ModelCheckpoint(dirpath=Path(config.checkpoint_folder, config.wandb_project,
+                                                         config.wandb_experiment_name),
+                                            filename='best_model:epoch={epoch:02d}-val_acc={val/acc:.4f}',
+                                            auto_insert_metric_name=False,
+                                            save_weights_only=True,
+                                            save_top_k=1),
+                        ])
+    trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=validation_loader)
