fixed a multitude of errors in custon mAP calculation

src/models/ssd_lightning.py
@@ -48,7 +48,6 @@ class SSDLightning(pl.LightningModule):
         self.det_scores.extend(det_scores_batch)
         self.true_bboxes.extend(bboxes)
         self.true_classes.extend(classes)
-        self.true_difficulties.extend([torch.zeros(len(bboxes), dtype=torch.bool)])
         preds = []
         for det_boxes, det_labels, det_scores in (
                 zip(det_boxes_batch, det_labels_batch, det_scores_batch)):
@@ -67,8 +66,13 @@ class SSDLightning(pl.LightningModule):
     def on_validation_epoch_end(self):
         val_mAP = self.mean_average_precision.compute()["map"]
         self.log("val_mAP", val_mAP, on_epoch=True, prog_bar=True)
-        custom_map = calculate_mAP(self.det_boxes, self.det_labels, self.det_scores,
-                                   self.true_bboxes, self.true_classes, self.true_difficulties)
+        self.true_difficulties.extend([torch.zeros(len(box), dtype=torch.bool,
+                                                   device=self.device)
+                                       for box in self.true_classes])
+
+        custom_APs, custom_map = calculate_mAP(self.det_boxes, self.det_labels, self.det_scores,
+                                   self.true_bboxes, self.true_classes,
+                                   self.true_difficulties, self.device)
         self.log("custom_map", custom_map, on_epoch=True, prog_bar=True)
         self.det_boxes.clear()
         self.det_labels.clear()

src/utils.py
@@ -99,7 +99,7 @@ def decimate(tensor, m):
 
 
 def calculate_mAP(det_boxes, det_labels, det_scores, true_boxes, true_labels,   # noqa: N802
-                  true_difficulties):
+                  true_difficulties, device):
     """
     Calculate the Mean Average Precision (mAP) of detected objects.
 
@@ -118,6 +118,7 @@ def calculate_mAP(det_boxes, det_labels, det_scores, true_boxes, true_labels,   
     one tensor for each image containing actual objects' labels
     :param true_difficulties: list of tensors,
     one tensor for each image containing actual objects' difficulty (0 or 1)
+    :param device: device on which the tensors are
     :return: list of average precisions for all classes, mean average precision (mAP)
     """
     assert len(det_boxes) == len(det_labels) == len(det_scores) == len(
@@ -132,7 +133,7 @@ def calculate_mAP(det_boxes, det_labels, det_scores, true_boxes, true_labels,   
     true_images = []
     for i in range(len(true_labels)):
         true_images.extend([i] * true_labels[i].size(0))
-    true_images = torch.LongTensor(true_images)
+    true_images = torch.tensor(true_images, dtype=torch.long, device=device)
     # (n_objects), n_objects is the total no. of objects across all images
     true_boxes = torch.cat(true_boxes, dim=0)  # (n_objects, 4)
     true_labels = torch.cat(true_labels, dim=0)  # (n_objects)
@@ -145,7 +146,7 @@ def calculate_mAP(det_boxes, det_labels, det_scores, true_boxes, true_labels,   
     det_images = []
     for i in range(len(det_labels)):
         det_images.extend([i] * det_labels[i].size(0))
-    det_images = torch.LongTensor(det_images)  # (n_detections)
+    det_images = torch.tensor(det_images, dtype=torch.long, device=device)  # (n_detections)
     det_boxes = torch.cat(det_boxes, dim=0)  # (n_detections, 4)
     det_labels = torch.cat(det_labels, dim=0)  # (n_detections)
     det_scores = torch.cat(det_scores, dim=0)  # (n_detections)
@@ -162,8 +163,7 @@ def calculate_mAP(det_boxes, det_labels, det_scores, true_boxes, true_labels,   
         true_class_boxes = true_boxes[true_labels == c]  # (n_class_objects, 4)
         true_class_difficulties = true_difficulties[
             true_labels == c]  # (n_class_objects)
-        n_easy_class_objects = (
-                    1 - true_class_difficulties).sum().item()
+        n_easy_class_objects = (~true_class_difficulties).sum().item()
         # ignore difficult objects
 
         # Keep track of which true objects with this class have already been 'detected'
@@ -215,7 +215,8 @@ def calculate_mAP(det_boxes, det_labels, det_scores, true_boxes, true_labels,   
             # image-level tensors 'object_boxes', 'object_difficulties'
             # In the original class-level tensors
             # 'true_class_boxes', etc., 'ind' corresponds to object with index...
-            original_ind = torch.LongTensor(range(true_class_boxes.size(0)))[
+            original_ind = torch.tensor(range(true_class_boxes.size(0)),
+                                        dtype=torch.long, device=device)[
                 true_class_images == this_image][ind]
             # We need 'original_ind' to update 'true_class_boxes_detected'
 
@@ -253,7 +254,7 @@ def calculate_mAP(det_boxes, det_labels, det_scores, true_boxes, true_labels,   
         # Find the mean of the maximum of the precisions
         # corresponding to recalls above the threshold 't'
         recall_thresholds = torch.arange(start=0, end=1.1, step=.1).tolist()  # (11)
-        precisions = torch.zeros((len(recall_thresholds)), dtype=torch.float)  # (11)
+        precisions = torch.zeros((len(recall_thresholds)), dtype=torch.float, device=device)  # (11)
         for i, t in enumerate(recall_thresholds):
             recalls_above_t = cumul_recall >= t
             if recalls_above_t.any():