936fc49fe38b718768927ad49cba451487b7a94d
Moritz Palm <moritz.palm@web.de>
2024-04-30 20:43:33 +0200

    changed tensor contruction back to legacy way

--- src/models/ssd.py
+++ src/models/ssd.py
@@ -620,18 +620,14 @@ class SSD300(nn.Module):
                 # Store only unsuppressed boxes for this class
                 image_boxes.append(class_decoded_locs[1 - suppress])
                 image_labels.append(
-                    torch.LongTensor((1 - suppress).sum().item() * [c],
-                                     device=self.priors_cxcy.device))
+                    torch.LongTensor((1 - suppress).sum().item() * [c]).to(self.priors_cxcy.device))
                 image_scores.append(class_scores[1 - suppress])
 
             # If no object in any class is found, store a placeholder for 'background'
             if len(image_boxes) == 0:
-                image_boxes.append(torch.FloatTensor([[0., 0., 1., 1.]],
-                                                     device=self.priors_cxcy.device))
-                image_labels.append(torch.LongTensor([0],
-                                                     device=self.priors_cxcy.device))
-                image_scores.append(torch.FloatTensor([0.],
-                                                      device=self.priors_cxcy.device))
+                image_boxes.append(torch.FloatTensor([[0., 0., 1., 1.]]).to(self.priors_cxcy.device))
+                image_labels.append(torch.LongTensor([0]).to(self.priors_cxcy.device))
+                image_scores.append(torch.FloatTensor([0.]).to(self.priors_cxcy.device))
 
             # Concatenate into single tensors
             image_boxes = torch.cat(image_boxes, dim=0)  # (n_objects, 4)
