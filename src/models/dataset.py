cb2469ba0aac9321ff995c5a7d4ae2175dee524f
upveo <upveo@student.kit.edu>
2024-05-03 17:43:53 +0200

    updated visualization

--- src/models/dataset.py
+++ src/models/dataset.py
@@ -9,7 +9,7 @@ from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import tv_tensors
 from torchvision.transforms import v2
 
-from utils import get_relative_coords, read_labels
+from src.utils import get_relative_coords, read_labels
 
 # TODO: check if migration from torchvision to
 #  albumentations for bounding box transformations is necessary
